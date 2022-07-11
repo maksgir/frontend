c8aafa40530c95b8b75cca43dfc23cc5cbfcd1ab
Максим <maksgirya@mail.ru>
2022-07-12 01:30:05 +0300

    отображение всех задач (только названия)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './views/categories/categories.component';
+import { TaskComponent } from './views/tasks/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/views/tasks/task.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Task} from "../../model/Task";
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  tasks: Task[] | undefined;
+
+  constructor(private service: DataHandlerService) { }
+
+  ngOnInit(): void {
+    this.tasks = this.service.getTasks();
+  }
+
+}
